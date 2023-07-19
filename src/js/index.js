f54fb92a0983506403ccb90570d6b280edad9db5
German Bystrov <g220794@gmail.com>
2023-07-19 11:53:03 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import '../scss/style.scss'
+import './_burgerBtn'
+import './_nextBtnMain'
+import './_callbackCloseBtn'
+import './_feedbackCloseBtn'
+import './_brandsBtnt'
+import './_devisesBtn'
+import './_swiper.js'
+
+console.log('Works!')
