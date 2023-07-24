2598420b33113e523a30e6d75aa9cc54a916cebc
German Bystrov <g220794@gmail.com>
2023-07-24 10:02:19 +0300

    Updates

--- src/js/_nextBtnMain.js
+++
@@ -1,7 +0,0 @@
-let aboutCompanyNextBtn = document.querySelector('.aboutcompany__btn')
-let aboutCompanyText = document.querySelector('.aboutcompany__text')
-
-aboutCompanyNextBtn.addEventListener('click', function () {
-  aboutCompanyNextBtn.classList.add('aboutcompany__btn--open')
-  aboutCompanyText.classList.add('text--open')
-})
